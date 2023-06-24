2ff3eaded2cdaee3617ca719d7ed3a02127adf18
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-23 22:05:43 -0500

    Estructura inicial del proyecto a refactorizar.

---
+++ src/co/com/Refactor/SolucionInicial/Main.java
@@ -0,0 +1,12 @@
+package co.com.Refactor.SolucionInicial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Comparendo comparendo =  new Comparendo();
+        comparendo.construirFotoMulta(80, "CARRO");
+        comparendo.construirFotoMulta(200, "MULA");
+        comparendo.construirFotoMulta(40, "CAMION");
+        comparendo.construirFotoMulta(40, "AVION");
+    }
+}
